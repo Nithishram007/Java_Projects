4271a22dacf0b6a37218a1b8620c085ed80e94fe
PrabhaBas <prabhavathibaskar088@gmail.com>
2020-08-08 15:08:44 +0530

    Initial Commit

--- pkbanking/pkinterface/InterestRate.java
+++
@@ -1,7 +0,0 @@
-package pkbanking.pkinterface;
-
-public interface InterestRate {
-    double sbrate=4.0/(12*100);
-    double fdrate=8.25/(12*100);
-
-}
--- pkbanking/pkinterface/Transaction.java
+++
@@ -1,7 +0,0 @@
-package pkbanking.pkinterface;
-
-public interface Transaction {
-    double min_balance = 500;
-    void withdraw(double a);
-    void deposit(double b);
-}
